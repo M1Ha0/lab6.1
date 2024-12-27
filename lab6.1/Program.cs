e0a5722a4c4cad6f2948ad78d56a6fc0dc3757ee
MiHa0_o <MiHa0_o@209-8>
2024-12-27 12:40:02 +0200

    Добавьте файлы проекта.

---
+++ lab6.1/Program.cs
@@ -0,0 +1,27 @@
+﻿Random random = new Random();
+int[,] mas = new int[4, 4];
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas[i, j] = random.Next(1, 16);
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        if (i > j)
+        {
+            mas[i, j] = 0;
+            Console.Write(mas[i, j] + " ");
+        }
+        else
+        {
+            Console.Write(mas[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
